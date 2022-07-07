d9927ce366395df107ee2c01bd2ee2e741221b03
igal abachi <igalk474@gmail.com>
2022-07-08 01:44:29 +0300

    .

--- repositories/pg.go
+++ repositories/pg.go
@@ -1,10 +1,11 @@
 package repositories
 
-const dbname = ""
-const host = ""
-const port = ""
-const user = ""
-const password = ""
+const dbname = "d66kr61430h4e"
+const host = "ec2-34-194-73-236.compute-1.amazonaws.com"
+const port = "5432"
+
+const user = "llbavbgsduhpsz"
+const password = "8698120fda348c13ecb5b051ddbddd9fc3a5fb7016895eb3e77c5747836079cd"
 const sslmode = "require"
 
 //os.Getenv("DATABASE_URL")
